0871e79933987e138c2b2220c96c3ca4bb7a79a6
Peizhe Zhang <zjuzpz@gmail.com>
2015-12-08 17:05:11 -0800

    Create print32ch.py

---
+++ project/print32ch.py
@@ -0,0 +1,23 @@
+for i in range(1):
+    print("NLC_1ch ch{} (.clk(clk),.reset(reset),.srdyi(srdyi),.srdyo(srdyo),.x_adc(ch{}_x_adc),\n\
+	.x_lin(ch{}_x_lin),.section_limit(ch{}_section_limit),.recip_stdev_1(ch{}_recip_stdev_1),\n\
+	.recip_stdev_2(ch{}_recip_stdev_2),.recip_stdev_3(ch{}_recip_stdev_3),.recip_stdev_4(ch{}_recip_stdev_4),\n\
+	.neg_mean_1(ch{}_neg_mean_1),.neg_mean_2(ch{}_neg_mean_2),.neg_mean_3(ch{}_neg_mean_3),\n\
+	.neg_mean_4(ch{}_neg_mean_4),\n\
+	.coeff_1_0(ch{}_coeff_1_0),.coeff_1_1(ch{}_coeff_1_1),.coeff_1_2(ch{}_coeff_1_2),.coeff_1_3(ch{}_coeff_1_3),\n\
+	.coeff_1_4(ch{}_coeff_1_4),.coeff_1_5(ch{}_coeff_1_5),\n\
+	.coeff_1_6(ch{}_coeff_1_6),.coeff_1_7(ch{}_coeff_1_7),.coeff_1_8(ch{}_coeff_1_8),.coeff_1_9(ch{}_coeff_1_9),\n\
+	.coeff_1_10(ch{}_coeff_1_10),.coeff_2_0(ch{}_coeff_2_0),\n\
+	.coeff_2_1(ch{}_coeff_2_1),.coeff_2_2(ch{}_coeff_2_2),.coeff_2_3(ch{}_coeff_2_3),.coeff_2_4(ch{}_coeff_2_4),\n\
+	.coeff_2_5(ch{}_coeff_2_5),.coeff_2_6(ch{}_coeff_2_6),\n\
+	.coeff_2_7(ch{}_coeff_2_7),.coeff_2_8(ch{}_coeff_2_8),.coeff_2_9(ch{}_coeff_2_9),.coeff_2_10(ch{}_coeff_2_10),\n\
+	.coeff_3_0(ch{}_coeff_3_0),.coeff_3_1(ch{}_coeff_3_1),\n\
+	.coeff_3_2(ch{}_coeff_3_2),.coeff_3_3(ch{}_coeff_3_3),.coeff_3_4(ch{}_coeff_3_4),.coeff_3_5(ch{}_coeff_3_5),\n\
+	.coeff_3_6(ch{}_coeff_3_6),.coeff_3_7(ch{}_coeff_3_7),\n\
+	.coeff_3_8(ch{}_coeff_3_8),.coeff_3_9(ch{}_coeff_3_9),.coeff_4_0(ch{}_coeff_4_0),.coeff_4_1(ch{}_coeff_4_1),\n\
+	.coeff_4_2(ch{}_coeff_4_2),.coeff_4_3(ch{}_coeff_4_3),\n\
+	.coeff_4_4(ch{}_coeff_4_4),.coeff_4_5(ch{}_coeff_4_5),.coeff_4_6(ch{}_coeff_4_6),.coeff_4_7(ch{}_coeff_4_7),\n\
+	.coeff_4_8(ch{}_coeff_4_8),.coeff_4_9(ch{}_coeff_4_9),\n\
+	.coeff_4_10(ch{}_coeff_4_10),.coeff_3_10(ch{}_coeff_3_10));".format(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,\
+        i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i))
+    print("\n")
